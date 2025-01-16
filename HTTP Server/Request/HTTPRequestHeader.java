eb336e2740b7f2525c412f71bf3e417f603dca77
Shiven48 <shevenroyal@gmail.com>
2025-01-16 09:12:21 +0530

    Added BodyBuilderImpl class and HttpRequestHeader class

--- HTTP Server/Request/HTTPRequestHeader.java
+++ HTTP Server/Request/HTTPRequestHeader.java
@@ -1,17 +1,17 @@
 package Request;
 
-import Interfaces.RequestType;
+import Builder.BodyBuilderImpl;
+import Builder.HeaderBuilderImpl;
+import Builder.RequestLineBuilderImpl;
 
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HTTPRequestHeader<T> {
 
+    private RequestLineBuilderImpl requestLine;
+    private HeaderBuilderImpl header;
+    private BodyBuilderImpl<T> body;
 
-
-//
-//
-//
-//    private ConcurrentHashMap<String, ResourceData<T>> resources;
+    private ConcurrentHashMap<String, ResourceData<T>> resources;
 
 }
