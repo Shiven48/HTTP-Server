beaa5bba1833ad21506feca28b4efb72e0a490ae
Shiven48 <shevenroyal@gmail.com>
2025-01-16 08:48:33 +0530

    Added RequestLineBuilderImpl class

--- HTTP Server/Request/HTTPRequestHeader.java
+++ HTTP Server/Request/HTTPRequestHeader.java
@@ -1,9 +1,30 @@
 package Request;
 
+import Interfaces.RequestType;
+
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HTTPRequestHeader<T> {
 
-    private ConcurrentHashMap<String, ResourceData<T>> resources;
+
+
+//    private Enum<RequestType> requestType;
+//
+//    private String requestResourcePath;
+//
+//    private String requestVersion;
+//
+//    private String host;
+//
+//    private int port;
+//
+//    private String contentType;
+//
+//    private List<String> acceptFormats;
+//
+//    private String userAgent;
+//
+//    private ConcurrentHashMap<String, ResourceData<T>> resources;
 
 }
