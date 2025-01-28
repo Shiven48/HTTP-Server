00d9082a7041dfca8747c41233f3e1fa5b3f638d
Shiven48 <shevenroyal@gmail.com>
2025-01-28 07:40:45 +0530

    Added parsing to url Note: More needed in case user passed params

--- HTTP Server/Server/Implementation/RequestLineHandler.java
+++ HTTP Server/Server/Implementation/RequestLineHandler.java
@@ -1,26 +1,35 @@
 package Implementation;
 
+import Common.Enums.Formats;
 import Common.Enums.RequestType;
-import Common.RequestLine;
-import Implementation.Builder.RequestLineBuilder;
+
+import java.util.Arrays;
 
 public class RequestLineHandler {
 
-    private RequestType requestType;
+    public native void initialize();
+    public native void insert(String path, String data);
+    public native String lookup(String path);
 
-    public RequestLineHandler(){ }
+    private RequestType requestType;
+    private String format;
+
+    static {
+        System.loadLibrary("trie");
+    }
+
+    public RequestLineHandler(){}
 
     // POST /some/path HTTP/1.1
     public void processRequestLine(String RequestLine){
         String[] requestLineComponents = RequestLine.split(" ");
         if(requestLineComponents.length != 3){
-            // Error response code
+            // Error response code Invalid request
         }
         parseFirstComponent(requestLineComponents[0]);
         parseSecondComponent(requestLineComponents[1]);
     }
 
-    // validation and parsing First Component
     private void parseFirstComponent(String component){
         String response = "";
         if(component != null && !component.isEmpty()){
@@ -45,22 +54,47 @@ public class RequestLineHandler {
         }
     }
 
-    // some/path/
-    // validation and parsing Second Component
-    private void parseSecondComponent(String component){
-        // Check in hash map that the key is present
-        // If present then parse on '/' character search the resource in tree based on String arr we get
-        //
+    // some/path/sometime.txt
+    private void parseSecondComponent(String unProcessedPath){
+        StringBuilder processedUrl = new StringBuilder();
+        if(unProcessedPath == null){
+            // Error response code Invalid request
+            return;
+        }
+        Arrays.stream(unProcessedPath.split("/"))
+                .map(url -> {
+                    if(url.contains(".")){
+                        String[] fileName_and_format = url.split("\\.");
+                        if(fileName_and_format.length == 2){
+                            String file = fileName_and_format[0].trim().toLowerCase();
+                            format = checkFormat(fileName_and_format[1].trim().toLowerCase());
+                            System.out.println("File is "+file);
+                            System.out.println("Format is "+format);
+                            return file;
+                        }
+                    }
+                    return url.trim().toLowerCase();
+                })
+                .forEach(processedUrl::append);
+        System.out.println(new String(processedUrl));
+//        handlePostRequest(new String(processedUrl),unProcessedPath);
     }
 
-    // validation and parsing Third Component
-    private void parseThirdComponent(String component){
-
+    private String checkFormat(String url){
+         return switch (url) {
+            case "txt" -> Formats.TXT.toString().trim().toLowerCase();
+            case "json" -> Formats.JSON.toString().trim().toLowerCase();
+            default -> throw new IllegalStateException("Unexpected value: " + url);
+        };
     }
+
+    private void parseThirdComponent(String component){}
 
     private void handleGetRequest(){}
 
-    private void handlePostRequest(){}
+    private void handlePostRequest(String processedPath, String unProcessedPath){
+        // save using trie
+    }
 
     private void handlePutRequest(){}
 
